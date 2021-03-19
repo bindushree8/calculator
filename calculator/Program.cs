a160e5f6bc71055dddc1d7926ff2c5517150d079
bindushree8 <80895783+bindushree8@users.noreply.github.com>
2021-03-19 14:31:53 +0530

    Update Program.cs

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -1,61 +1,73 @@
 using System;
-public class Program
+
+namespace calculatorinterface
 {
-	public int add(int x, int y)
-	{
-		int result = x + y;
-		return result;
-	}
+    public interface Iinterface1
+    {
+        void add(int a, int b);
+        void sub(int a, int b);
+        void mul(int a, int b);
+        void divi(double a, double b);
+        void display();
+    }
+    class inter : Iinterface1
+    {
+        int x, y, z;
+        double d;
+        public void add(int a, int b)
+        {
+            int m, n;
+            m = a;
+            n = b;
+            x = m + n;
+        }
+        public void sub(int a, int b)
+        {
+            int m, n;
+            m = a;
+            n = b;
+            y = a - b;
+        }
+        public void mul(int a, int b)
+        {
+            int m, n;
+            m = a;
+            n = b;
+            z = a * b;
+        }
+        public void divi(double a, double b)
+        {
+            double m, n;
+            m = a;
+            n = b;
+            d = a / b;
+        }
+        public void display()
+        {
+            Console.WriteLine("Addition value is:" + x);
+            Console.WriteLine("Subtraction value is:" + y);
+            Console.WriteLine("Multiplication value is:" + z);
+            Console.WriteLine("Divition value is:" + d);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            inter obj = new inter();
+            int g, h;
 
-		public int sub(int x, int y)
-		{
-			int result = x - y;
-			return result;
-		}
 
-		public int mul(int x, int y)
-		{
-			int result = x * y;
-			return result;
-		}
-
-	public int div(int x, int y)
-	{
-		int result = x / y;
-		return result;
-	}
-
-
-	public static void Main(string[] args)
-	{
-		int x, y, s;
-
-		Console.Write("Enter first number:");
-        x = Convert.ToInt32(Console.ReadLine());
-
-		Console.Write("Enter second number:");
-		y = Convert.ToInt32(Console.ReadLine());
-
-		Program obj1 = new Program();
-		s = obj1.add(x, y);
-		Console.WriteLine("Sum is:" + s);
-
-
-
-		Program obj2 = new Program();
-		s = obj2.sub(x, y);
-		Console.WriteLine("Subtraction  is:" + s);
-
-
-		Program obj3 = new Program();
-		s = obj3.mul(x, y);
-		Console.WriteLine("multiplcation is:" + s);
-
-
-		Program obj4 = new Program();
-		s = obj4.div(x, y);
-		Console.WriteLine("division is:" + s);
-
-		
-	}
+            Console.WriteLine("Enter the first Number to perform calculator operations:");
+            g = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter the second Number to perform calculator operations:");
+            h = Convert.ToInt16(Console.ReadLine());
+            obj.add(g, h);
+            obj.sub(g, h);
+            obj.mul(g, h);
+            obj.divi(g, h);
+            obj.display();
+            Console.ReadKey();
+        }
+    }
 }
